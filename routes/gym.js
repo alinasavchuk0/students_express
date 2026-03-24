791b669a08af224e7ad7874b3c8802eb37b0a09c
Віталій Дмитрик <dmytrykv2008@gmail.com>
2026-03-24 12:32:52 +0200

    Implement exercise addition routes
    Add routes for adding exercises to the gym database.

--- routes/gym.js
+++ routes/gym.js
@@ -13,5 +13,38 @@ router.get('/', async function(req, res, next) {
   })
   res.render('gym', { exercise: modExercise || [] });
 });
+router.get('/addExercise', async function(req, res, next) {
+  res.render('forms/gym_form');
+})
+
+router.post('/addExercise', async function(req, res, next) {
+  console.log("Submitted data: ", req.body);
+
+const { exercise_name, difficult_level, required_level, Muscle_name, Sets } = req.body;
+
+  async function addExer(exercise_name, difficult_level, required_level, Muscle_name, Sets) {
+   try {
+      const query = `
+      INSERT INTO gym (
+            exercise_name, difficult_level, required_level, Muscle_name, Sets
+        )
+        VALUES ($1, $2, $3, $4, $5) 
+        RETURNING *`;
+   const res = await db.query(query, [exercise_name, difficult_level, required_level, Muscle_name, Sets]);
+
+   } catch (err) 
+      { console.error(err)
+        throw err;
+   }
+}
+
+try {
+    await addExer(exercise_name, difficult_level, required_level, Muscle_name, Sets);
+    
+    res.redirect('/gym');
+  } catch (err) {
+    res.status(500).send("Помилка при додаванні вправи. Можливо, вона вже існує.");
+  }
+});
 
 export default router;
