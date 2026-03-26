0a6659bae59c62f7a6b36b664d039e3e60b14a4d
classuser51 <classuser51@local.iful.edu.ua>
2026-03-26 11:24:42 +0200

    lesson

--- db/connector.js
+++ db/connector.js
@@ -8,5 +8,52 @@ const pool = new Pool({
 });
 
 //Add table creation
+const createTableQueries = [];
+
+createTableQueries.push(`
+    CREATE TABLE IF NOT EXISTS heroes1 (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,              
+    primary_attribute TEXT,        
+    role TEXT,       
+    attack_type TEXT,           
+    difficulty INTEGER,                
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+   `);
+createTableQueries.push(`
+    CREATE TABLE IF NOT EXISTS sloniki1 (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    age TEXT NOT NULL,
+    place_of_birth TEXT NOT NULL,           
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+   );
+      `);
+
+
+for (let i = 0 ; i < createTableQueries.length ; i++){
+    console.log(`create string`)
+    pool.query(createTableQueries[i]);
+}
+
+
+      
+// const initializeDatabase = async () => {
+//    console.log('-----------------------------------------------');
+//    console.log('Initializing sloniki database...');
+
+
+//    try {
+//       await 
+//    } catch (error) {
+//       console.error('!! Error initializing database');
+//       // console.error('Full error:', error);
+//       throw error;
+//    }
+// };
+
+console.log("CONNECTED!!!! ✔")
 
 export default pool;
